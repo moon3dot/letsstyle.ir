2f5105ef028faee8c0965b71db1a675f8be46e77
moon3dot <moon3dot@gmail.com>
2024-01-20 14:45:09 +0330

    fix uri in file

---
+++ wp-content/themes/hello-elementor-child/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/assets/boot/css/bootstrap.rtl.min.css" />
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/style.css" />
+    <script src="./assets/boot/js/bootstrap.bundle.min.js" defer></script>
+    <script src="./assets/js/main.js" defer></script>
+    <script src="assets/js/swiper-bundle.min.js" defer></script>
+    <script src="assets/js/javascript.js" defer></script>
+    <title>Document</title>
+  </head>
