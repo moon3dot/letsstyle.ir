fe53def4908185d60f84890182e92b7e4d3bf7ce
moon3dot <moon3dot@gmail.com>
2024-01-22 15:01:10 +0330

    del plugin & add plugin elementor

--- wp-content/themes/hello-elementor-child/header.php
+++ wp-content/themes/hello-elementor-child/header.php
@@ -1,24 +1,43 @@
-<!DOCTYPE html>
+<?php
+/**
+ * The template for displaying the header
+ *
+ * This is the template that displays all of the <head> section, opens the <body> tag and adds the site's header.
+ *
+ * @package HelloElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+$viewport_content = apply_filters( 'hello_elementor_viewport_content', 'width=device-width, initial-scale=1' );
+$enable_skip_link = apply_filters( 'hello_elementor_enable_skip_link', true );
+$skip_link_url = apply_filters( 'hello_elementor_skip_link_url', '#content' );
+?>
+<!doctype html>
 <html <?php language_attributes(); ?>>
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
-    />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/swiper-bundle.min.css" />
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/assets/boot/css/bootstrap.rtl.min.css" />
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/style.css" />
-    <script src="./assets/boot/js/bootstrap.bundle.min.js" defer></script>
-    <script src="./assets/js/main.js" defer></script>
-    <script src="assets/js/swiper-bundle.min.js" defer></script>
-    <script src="assets/js/javascript.js" defer></script>
-    <title>Document</title>
-  </head>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<?php wp_body_open(); ?>
+
+<?php if ( $enable_skip_link ) { ?>
+<a class="skip-link screen-reader-text" href="<?php echo esc_url( $skip_link_url ); ?>"><?php echo esc_html__( 'Skip to content', 'hello-elementor' ); ?></a>
+<?php } ?>
+
+<?php
+if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'header' ) ) {
+	if ( hello_elementor_display_header_footer() ) {
+		if ( did_action( 'elementor/loaded' ) && hello_header_footer_experiment_active() ) {
+			get_template_part( 'template-parts/dynamic-header' );
+		} else {
+			get_template_part( 'template-parts/header' );
+		}
+	}
+}
